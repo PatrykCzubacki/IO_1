bcbf57630bca9cc3e7be1e9c9347d81ea89089ee
Patryk Czubacki <patryk.i.czubacki@gmail.com>
2025-11-27 20:39:35 +0100

    Enlarging the player - Fix #2

--- public/client.js
+++ public/client.js
@@ -156,7 +156,7 @@ function draw() {
     // Draw
     ctx.fillStyle = r.color;
     ctx.beginPath();
-    ctx.arc(r.x, r.y, 10, 0, Math.PI * 2);
+    ctx.arc(r.x, r.y, 24, 0, Math.PI * 2);
     ctx.fill();
   }
   requestAnimationFrame(draw);
