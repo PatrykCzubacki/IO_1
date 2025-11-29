92a0f5408607968cf9812743e051eb8c7d0d0041
Patryk Czubacki <patryk.i.czubacki@gmail.com>
2025-11-29 23:04:14 +0100

    Add animation to text - Fix #4

--- public/client.js
+++ public/client.js
@@ -60,8 +60,8 @@ document.addEventListener('keydown', (e) => {
 if (key === 'x' && renderPlayers[socket.id]){
   floatingText = {
     text: "BOOO!",
-    timer: 500, // miliseconds
-    duration: 500 // store original duration
+    timer: 250, // current remaining time in ms
+    duration: 250 // total time for animation in ms
   };
 }
 });
